70b6f050fafa2db9707b5106cbd202b78371c2bd
git-mahade <git-mahade@devxhub.com>
2023-11-20 19:51:04 +0600

    modify-button-and-input-component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
-  <!-- <main class="flex justify-center items-center h-screen w-full">
+  <main class="flex justify-center items-center h-screen w-full">
     <p class="text-5xl italic"><span class="text-red-600">Devxhub</span> component library</p>
-  </main> -->
-  <div  class="ml-20">
-    <DButton size="md" color="white" rounded="0"  variant="outlined" block>BUTTON</DButton>
-  </div>
+  </main>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import DButton from './components/DButton.vue'
-</script>
