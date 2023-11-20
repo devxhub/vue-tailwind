adfc5fe8edf46870778c6570632a45c8ca88ccd4
git-mahade <git-mahade@devxhub.com>
2023-11-20 19:46:35 +0600

    modify-button-and-input-component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <!-- <main class="flex justify-center items-center h-screen w-full">
     <p class="text-5xl italic"><span class="text-red-600">Devxhub</span> component library</p>
   </main> -->
-  <DButton size="md" color="white" rounded="0"  class="m-20" variant="outlined">BUTTON</DButton>
+  <div  class="ml-20">
+    <DButton size="md" color="white" rounded="0"  variant="outlined" block>BUTTON</DButton>
+  </div>
 </template>
 
 <script setup lang="ts">
