b672ac8395f8f3d0d75edc04700534cd1a66932e
git-mahade <git-mahade@devxhub.com>
2023-11-20 18:29:08 +0600

    modify-button-and-input-component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <main class="flex justify-center items-center h-screen w-full">
+  <!-- <main class="flex justify-center items-center h-screen w-full">
     <p class="text-5xl italic"><span class="text-red-600">Devxhub</span> component library</p>
-  </main>
+  </main> -->
+  <DInput v-model="text" :placeholder="'test'" type="email" class="mx-32 mt-6" />
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import DInput from './components/DInput.vue'
+const text = ref('')
+</script>
--- src/components/DInput.vue
+++ src/components/DInput.vue
@@ -1,38 +1,82 @@
 <template>
-  <label :for="id" class="block">
-    <span class="text-gray-700">{{ label }}</span>
+  <div
+    class="relative mt-4 transition-all duration-300 pt-4 rounded-t-md"
+    :class="[
+      disabled ? 'opacity-50 hover:bg-gray-100' : '',
+      { 'bg-gray-100 hover:bg-gray-200 px-4': variant === 'default' },
+      { 'bg-transparent px-0': variant === 'underlined' }
+    ]"
+  >
     <input
       :id="id"
       v-model="inputValue"
       :type="type"
-      :placeholder="placeholder"
+      :placeholder="isFocused ? placeholder : ''"
       :disabled="disabled"
-      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+      class="w-full py-1 focus:outline-none focus:border-transparent bg-transparent"
+      :class="[{ 'focus:border-black': isFocused }]"
+      @focus="isFocused = true"
+      @blur="handleBlur"
       @input="$emit('update:modelValue', inputValue)"
     />
-  </label>
+    <label
+      v-if="label"
+      class="absolute top-3 text-gray-600 text-md transform origin-left transition-transform duration-150 pointer-events-none"
+      :class="[
+        { '-translate-y-3 scale-75': isFocused },
+        { 'left-4': variant === 'default' },
+        { 'left-0': variant === 'underlined' }
+      ]"
+      for="my-input"
+    >
+      {{ label }}
+    </label>
+    <div
+      v-if="isFocused"
+      class="border-b-2 border-black absolute bottom-0 left-0 right-0 transition-all duration-300"
+    ></div>
+    <div v-else class="border-b border-gray-600 absolute bottom-0 left-0 right-0"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-const props = defineProps({
-  modelValue: String,
-  label: String,
-  id: String,
+const { id, modelValue, label, type, placeholder, disabled } = defineProps({
+  id: {
+    type: String
+  },
+  modelValue: {
+    type: String
+  },
+  label: {
+    type: String,
+    default: 'Label'
+  },
   type: {
     type: String,
-    default: 'text',
+    default: 'text'
   },
-  placeholder: String,
-  disabled: Boolean,
-});
+  placeholder: {
+    type: String
+  },
+  variant: {
+    type: String,
+    default: 'default'
+  },
+  disabled: {
+    type: Boolean
+  }
+})
 
-const inputValue = ref(props.modelValue);
+const inputValue = ref(modelValue)
+const isFocused = ref(modelValue !== '')
 
-inputValue.value = props.modelValue;
+const handleBlur = () => {
+  if (inputValue.value !== '') {
+    isFocused.value = true
+  } else {
+    isFocused.value = false
+  }
+}
 </script>
-
-<style scoped>
-/* Add any additional styles here */
-</style>
