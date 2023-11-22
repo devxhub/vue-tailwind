d56c5491eb262e0f92f1ce3706318f0571cf367f
git-mahade <git-mahade@devxhub.com>
2023-11-22 12:02:00 +0600

    modify-button-and-input-component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import DButton from './components/DButton.vue'
 import DTextInput from './components/DTextInput.vue'
-import DFileInput from './components/DFileInput.vue'
 
 export default { DButton, DTextInput, DFileInput }
