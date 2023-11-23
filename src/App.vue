29646504e83c3145a0e01fd6ca2a8197f3742a5a
git-mahade <git-mahade@devxhub.com>
2023-11-23 21:31:34 +0600

    text-input

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <main>Vue-Tailwind</main>
-  <DTextInput v-model="text" label="Label" @change="data" @enter="console.log('enter')" autofocus />
+  <DTextInput
+    class="mx-20"
+    v-model="text"
+    clearable
+    label="Label"
+    @change="data"
+    @focus="isFocused = true"
+    @blue="isFocused = false"
+  >
+    <!-- <div v-if="isFocused"></div> -->
+    <template #clear-icon >
+      <!-- <p>car</p> -->
+    </template>
+  </DTextInput>
 </template>
 
 <script setup>
@@ -8,6 +21,7 @@ import { ref } from 'vue'
 import DTextInput from './components/DTextInput.vue'
 
 const text = ref()
+const isFocused = ref(false)
 const data = (d) => {
   // console.log(d)
 }
--- src/components/DTextInput.vue
+++ src/components/DTextInput.vue
@@ -1,22 +1,34 @@
 <template>
   <label :for="id" class="block">
     <span>{{ label }}</span>
-    <input
-      :id="id"
-      v-model="inputValue"
-      :type="type"
-      :placeholder="placeholder"
-      :disabled="disabled"
-      :readonly="readonly"
-      :autofocus="autofocus"
-      :required="required"
-      class="w-full border"
-      @focus="$emit('focus')"
-      @blur="$emit('blur')"
-      @change="$emit('change')"
-      @keyup.enter="$emit('enter')"
-      @input="$emit('update:modelValue', inputValue)"
-    />
+    <div class="relative">
+      <input
+        :id="id"
+        v-model="inputValue"
+        :type="type"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        :readonly="readonly"
+        :autofocus="autofocus"
+        :required="required"
+        :clearable="clearable"
+        class="w-full border"
+        @focus="$emit('focus')"
+        @blur="$emit('blur')"
+        @change="$emit('change')"
+        @keyup.enter="$emit('enter')"
+        @input="$emit('update:modelValue', inputValue)"
+      />
+      <span v-if="clearable" class="absolute right-2 top-0 bottom-0 my-auto cursor-pointer">
+        <slot name="clear-icon">
+          <svg class="inline mb-[1px]" xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 0 512 512">
+            <path
+              d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"
+            />
+          </svg>
+        </slot>
+      </span>
+    </div>
   </label>
 </template>
 
@@ -36,8 +48,9 @@ const props = defineProps({
   readonly: Boolean,
   autofocus: Boolean,
   required: Boolean,
+  clearable: Boolean
 })
-const emit = defineEmits(['focus', 'blur', 'change','enter', 'update:modelValue'])
+const emit = defineEmits(['focus', 'blur', 'change', 'enter', 'update:modelValue'])
 
 const inputValue = ref(props.modelValue)
 
