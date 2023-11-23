5bc561622f41b5d22c562bec5495a8ed3b12e2a0
git-mahade <git-mahade@devxhub.com>
2023-11-23 21:32:59 +0600

    text-input

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,3 @@
 <template>
   <main>Vue-Tailwind</main>
-  <DTextInput
-    class="mx-20"
-    v-model="text"
-    clearable
-    label="Label"
-    @change="data"
-    @focus="isFocused = true"
-    @blue="isFocused = false"
-  >
-    <!-- <div v-if="isFocused"></div> -->
-    <template #clear-icon >
-      <!-- <p>car</p> -->
-    </template>
-  </DTextInput>
 </template>
-
-<script setup>
-import { ref } from 'vue'
-import DTextInput from './components/DTextInput.vue'
-
-const text = ref()
-const isFocused = ref(false)
-const data = (d) => {
-  // console.log(d)
-}
-</script>
