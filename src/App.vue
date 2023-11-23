0a994f9be72e029ee4803362fbab0ed93695daf2
git-mahade <git-mahade@devxhub.com>
2023-11-23 11:18:38 +0600

    global-change

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
-    Vue-Tailwind
+  <main class="flex justify-center items-center h-screen w-full">
+    <p class="text-5xl italic"><span class="text-red-600">Devxhub</span> component library</p>
   </main>
 </template>
