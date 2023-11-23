8f249afe7cb9ba565f6394ba1c64923a9c28d237
git-mahade <git-mahade@devxhub.com>
2023-11-23 11:50:30 +0600

    file-input

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <main class="flex justify-center items-center h-screen w-full">
     <p class="text-5xl italic"><span class="text-red-600">Devxhub</span> component library</p>
+<<<<<<< HEAD
   </main>
 </template>
+=======
+  </main> -->
+  <DButton size="md" color="white" rounded="0"  class="m-20" variant="outlined">BUTTON</DButton>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import DButton from './components/DButton.vue'
+</script>
+>>>>>>> 18e2d43 (modify-button-and-input-component)
