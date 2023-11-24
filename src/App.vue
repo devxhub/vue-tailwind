d6e0ed1056bf988f3bbe0779fb153066b9b46481
git-mahade <git-mahade@devxhub.com>
2023-11-24 23:20:44 +0600

    file-input

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,3 @@
 <template>
   <main>Vue-Tailwind</main>
-  <div class="mx-20">
-    <DFileInput v-model="file" multiple clearable>
-      <template #clear-icon></template>
-    </DFileInput>
-    <div v-for="fil of file">
-      <p>{{ fil.name }}</p>
-    </div>
-  </div>
 </template>
-
-<script setup>
-import DFileInput from './components/DFileInput.vue'
-import { ref } from 'vue'
-
-const file = ref(null)
-</script>
