42145da4dcb15abd4eac260c3b6f47b62a2572e1
git-mahade <git-mahade@devxhub.com>
2023-11-24 16:58:14 +0600

    text-input

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
 <template>
   <main>Vue-Tailwind</main>
-  <div class="mx-20">
-    <DTextInput clearable>
-      <template #clear-icon></template>
-    </DTextInput>
-  </div>
 </template>
-
-<script setup>
-import DTextInput from './components/DTextInput.vue'
-</script>
