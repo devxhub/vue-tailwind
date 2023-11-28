85bc984c2bc8cfb20b5caa7bec541e9b8b459e74
git-mahade <git-mahade@devxhub.com>
2023-11-28 11:28:57 +0600

    text-input

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
 <template>
   <main>Vue-Tailwind</main>
-  <div class="mx-20">
-    <DTextInput v-model="file" @enter="console.log('enter')" @blur="console.log('blur')" clearable>
-      <template #clear-icon></template>
-    </DTextInput>
-    <pre>{{ file?.size }}</pre>
-  </div>
 </template>
-
-<script setup>
-import DTextInput from './components/DTextInput.vue'
-import { ref } from 'vue'
-
-const file = ref(null)
-</script>
