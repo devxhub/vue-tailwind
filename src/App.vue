02d6fe2a993343cf2a425dfa89c9a36e272553ec
git-mahade <git-mahade@devxhub.com>
2023-11-29 12:19:58 +0600

    file-input

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
 <template>
   <main>Vue-Tailwind</main>
-  <DFileInput clearable/>
-  
 </template>
-
-<script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue'
-import DFileInput from './components/DFileInput.vue'
-
-
-</script>
