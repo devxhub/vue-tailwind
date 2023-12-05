5912fa27ed53240c456ac9f5e98affc0cd66277e
git-mahade <git-mahade@devxhub.com>
2023-12-05 15:45:59 +0600

    checkbox

---
+++ src/components/DxhCheckbox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex items-center space-x-1">
+    <input
+      :id="id"
+      type="checkbox"
+      :name="name"
+      :value="value"
+      :checked="defaultChecked ? defaultChecked : checked"
+      :disabled="disabled"
+      @change="handleChange"
+    />
+    <label :for="id" v-if="label">
+      <slot name="label">{{ label }}</slot>
+    </label>
+  </div>
+  <p v-if="hint">{{ hint }}</p>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+defineProps({
+  id: Number,
+  name: String,
+  value: String,
+  label: String,
+  checked: Boolean,
+  defaultChecked: Boolean,
+  hint: String,
+  disabled: Boolean
+})
+
+const emit = defineEmits(['change'])
+
+const handleChange = (event: any) => {
+  emit('change', event)
+}
+</script>
