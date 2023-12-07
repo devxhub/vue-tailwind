ad7d8ed6493e114316399776be457c83fdae068d
git-mahade <git-mahade@devxhub.com>
2023-12-07 14:31:26 +0600

    collapse

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [
+    vue(),
+    dts()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,11 +18,11 @@ export default defineConfig({
   },
   build: {
     lib: {
-      // Could also be a dictionary or array of accordion entry points
+      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'DEVxHUB',
       // the proper extensions will be added
-      fileName: 'devxhub-vue-tailwind'
+      fileName: 'devxhub-vue-tailwind',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
@@ -29,9 +32,9 @@ export default defineConfig({
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
