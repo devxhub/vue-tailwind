e3f5322ddf842626752879de587a6877fb71dbc1
git-mahade <git-mahade@devxhub.com>
2023-12-10 18:26:05 +0600

    tabs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <template>
-  <div id="app">
-    <TabComponent direction="horizontal" defaultActive="3" :items="tabsData" />
-  </div>
+  <main>
+    Vue-Tailwind
+  </main>
 </template>
-
-<script setup lang="ts">
-import TabComponent from './components/DxhTabs.vue'
-
-const tabsData = [
-  { id: 1, label: 'Tab 1', content: 'Content for Tab 1' },
-  { id: 2, label: 'Tab 2', content: 'Content for Tab 2' },
-  { id: 3, label: 'Tab 3', content: 'Content for Tab 3' },
-  { id: 4, label: 'Tab 4', content: 'Content for Tab 4' },
-  { id: 5, label: 'Tab 5', content: 'Content for Tab 5' }
-]
-</script>
