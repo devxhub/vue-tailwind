11307d7550155b89afedcf6180fe6ea4f971a9b9
git-mahade <git-mahade@devxhub.com>
2023-12-11 16:00:33 +0600

    modal

---
+++ src/components/__tests__/DxhModal.spec.ts
@@ -0,0 +1,67 @@
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+import { mount } from '@vue/test-utils'
+import DxhModal from '@/components/DxhModal.vue'
+
+describe('DxhModal.vue', () => {
+  let wrapper: any
+
+  beforeEach(() => {
+    wrapper = mount(DxhModal, {
+      props: {
+        title: 'Test Modal',
+        footer: true,
+        open: true,
+        keyboardEsc: true,
+        zIndex: 1000,
+        persistent: false
+      }
+    })
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+  })
+
+  it('renders modal with title and footer', () => {
+    const title = wrapper.find('.text-lg.font-bold.mb-4')
+    const footer = wrapper.find('.flex.space-x-2.justify-end')
+
+    expect(title.exists()).toBe(true)
+    expect(title.text()).toBe('Test Modal')
+    expect(footer.exists()).toBe(true)
+  })
+
+  it('emits cancel event on cancel button click', async () => {
+    const cancelButton = wrapper.find('[data-test="cancel-button"]')
+
+    await cancelButton.trigger('click')
+
+    const cancelEvent = wrapper.emitted('cancel')
+    expect(cancelEvent).toBeTruthy()
+  })
+
+  it('emits ok event on ok button click', async () => {
+    const okButton = wrapper.find('[data-test="ok-button"]')
+
+    await okButton.trigger('click')
+
+    const okEvent = wrapper.emitted('ok')
+    expect(okEvent).toBeTruthy()
+  })
+
+  it('closes modal on outside click', async () => {
+    const modalOverlay = wrapper.find('.bg-black.bg-opacity-50')
+
+    await modalOverlay.trigger('click')
+
+    const cancelEvent = wrapper.emitted('cancel')
+    expect(cancelEvent).toBeTruthy()
+  })
+
+  it('closes modal on pressing ESC key', async () => {
+    await wrapper.trigger('keydown.esc')
+
+    const cancelEvent = wrapper.emitted('cancel')
+    expect(cancelEvent).toBeUndefined()
+  })
+})
