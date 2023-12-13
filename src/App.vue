b34b469f5b12203a524a2035ca610b0919f6dd70
git-mahade <git-mahade@devxhub.com>
2023-12-13 15:47:34 +0600

    button

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,3 @@
 <template>
-  <div class="flex items-center">
-    <Button @click="handleButtonClick">Click me</Button>
-    <Button class="w-20" :loading="true">Loading</Button>
-    <Button :disabled="true">Disabled Button</Button>
-
-    <Button href="https://example.com" target="_blank">Visit Example</Button>
-    <Button href="https://example.com" target="_blank" :loading="true">Loading Link</Button>
-    <Button href="https://example.com" target="_blank" :disabled="true">Disabled Link</Button>
-  </div>
+  <main>Vue-Tailwind</main>
 </template>
-
-<script setup lang="ts">
-import Button from './components/DxhButton.vue';
-
-const handleButtonClick = () => {
-  console.log('Button clicked!');
-};
-</script>
