2afde8038bd2591cf34902ad9f5f36ca90bd90b3
git-mahade <git-mahade@devxhub.com>
2023-12-13 16:40:21 +0600

    extarea

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,3 @@
 <template>
-  <div>
-    <TextareaInput
-      v-model="textValue"
-      label="Your Label 2"
-      id="uniqueId"
-      rows="2"
-      cols="4"
-      maxlength="200"
-      placeholder="Enter your text here"
-      hint="Helpful hint goes here"
-      :disabled="false"
-      :resize="true"
-      :autofocus="false"
-      :required="true"
-      :clearable="true"
-    >
-    </TextareaInput>
-    <p>Parent Component Value: {{ textValue }}</p>
-  </div>
+  <main>Vue-Tailwind</main>
 </template>
-
-<script setup lang="ts">
-import TextareaInput from './components/DxhTextarea.vue'
-import { ref } from 'vue'
-
-const textValue = ref('')
-</script>
