5019cd4ae15bfddc07bf74a51c859ca92ddde842
git-mahade <git-mahade@devxhub.com>
2023-12-14 17:50:16 +0600

    skeleton-loader

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,5 @@
 <template>
-  <div>
-    <h1>Page Content</h1>
-    <div class="flex flex-col space-y-2">
-      <SkeletonBox mode="pulse" :animate="false"/>
-      <SkeletonBox mode="shimmer"/>
-      <SkeletonBox />
-      <SkeletonBox />
-
-      <SkeletonBox :rounded="true" :height="100" :width="100" :animate="false">
-      </SkeletonBox>
-      <SkeletonBox :height="150" :maxWidth="100" :minWidth="80" />
-    </div>
-  </div>
+  <main>
+    Vue-Tailwind
+  </main>
 </template>
-
-<script setup lang="ts">
-import SkeletonBox from './components/DxhSkeletonLoader.vue'
-</script>
