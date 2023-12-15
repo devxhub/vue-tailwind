2708e0ee6dfc0e41d6c5ddf5dbc8e845b6f86bfd
git-mahade <git-mahade@devxhub.com>
2023-12-15 16:38:26 +0600

    chips

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,3 @@
 <template>
-  <div class="m-8">
-    <closable-chip
-      :text="chipText"
-      :closeable="true"
-      classes="border border-red-400"
-      draggable
-      @close="console.log('cls')"
-      @drop="dropped"
-    >
-      <template #close="{ onClick }"><div @click="onClick">x</div></template>
-    </closable-chip>
-  </div>
+  <main>Vue-Tailwind</main>
 </template>
-
-<script setup lang="ts">
-import ClosableChip from './components/DxhChips.vue'
-
-const chipText = 'Chip'
-const dropped = (dd) => {
-  console.log(dd)
-}
-</script>
