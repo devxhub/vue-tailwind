1170f0cec93748dbc14f420f9b98fc4a9bc006b5
git-mahade <git-mahade@devxhub.com>
2023-12-18 11:37:23 +0600

    dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="p-8">
-    <Dropdown v-model="selectedItem" :items="dropdownItems" click  arrow></Dropdown>
-    <p class="mt-4">Selected Item: {{ selectedItem }}</p>
-  </div>
+  <main>
+    Vue-Tailwind
+  </main>
 </template>
-
-<script setup lang="ts">
-import Dropdown from './components/DxhDropdown.vue'
-import { ref } from 'vue'
-
-const dropdownItems = [
-  { id: '1', option: 'Option 1' },
-  { id: '2', option: 'Option 2' },
-  { id: '3', option: 'Option 3' }
-]
-const selectedItem = ref<string | null>(null)
-</script>
