78c45c90cc14454ad25d8f55eb05e62a0b139fc0
git-mahade <git-mahade@devxhub.com>
2023-12-19 12:57:38 +0600

    stepper

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="m-10">
     <StepComponent :steps="steps">
-      <template #content="{ currentStep }">
-        <div class="py-10" v-if="currentStep === 1">This is one man</div>
-        <div class="py-10" v-if="currentStep === 2">This is two man yooo</div>
-        <div class="py-10" v-if="currentStep === 3">This is three man yooo</div>
-        <div class="py-10" v-if="currentStep === 4">This is four man yooo</div>
+      <template #default="{ currentStep }">
+        <div v-if="currentStep === 0">This is one man</div>
+        <div v-if="currentStep === 1">This is two man yooo</div>
+        <div v-if="currentStep === 2">This is 3 man yooo</div>
+        <div v-if="currentStep === 3">This is 4 man yooo</div>
       </template>
     </StepComponent>
   </div>
@@ -15,10 +15,10 @@
 import StepComponent from './components/DxhSteps.vue'
 
 const steps = [
-  { id: 1, title: 'Step 1', subtitle: 'Subtitle 1', content: 'this is content 1' },
-  { id: 2, title: 'Step 2', subtitle: 'Subtitle 2', content: 'this is content 2' },
-  { id: 3, title: 'Step 3', subtitle: 'Subtitle 3', content: 'this is content 3' },
-  { id: 4, title: 'Step 4', subtitle: 'Subtitle 4', content: 'this is content 4' }
+  { title: 'Step 1', subtitle: 'Subtitle 1', content: 'this is content 1' },
+  { title: 'Step 2', subtitle: 'Subtitle 2', content: 'this is content 2' },
+  { title: 'Step 3', subtitle: 'Subtitle 3', content: 'this is content 3' },
+  { title: 'Step 4', subtitle: 'Subtitle 4', content: 'this is content 4' }
   // Add more steps as needed
 ]
 </script>
--- src/components/DxhSteps.vue
+++ src/components/DxhSteps.vue
@@ -1,62 +1,76 @@
 <template>
-  <div class="p-8 shadow-md">
+  <div class="p-8 shadow-md" data-test="your-component">
     <div class="flex items-center justify-between space-x-3 w-full">
-      <template v-for="(step, id) in steps" :key="id">
+      <template v-for="(step, index) in steps" :key="index">
         <div
           class="flex items-center justify-center space-x-2"
           :class="{
-            'text-gray-500': id >1 && id > currentStep,
-            'text-black': id <= currentStep
+            'text-gray-500': index > 0 && index > currentStep,
+            'text-black': index <= currentStep
           }"
+          :data-test="`step-${index}`"
         >
-          <div
-            class="rounded-full border"
-            :class="{
-              'border-gray-500 bg-white': id > 1 && id > currentStep,
-              'border-blue-500 bg-blue-500 text-white': id <= currentStep
-            }"
+          <slot name="step" :index="index" :currentStep="currentStep">
+            <div
+              class="rounded-full border"
+              :class="{
+                'border-gray-500 bg-white': index > 0 && index > currentStep,
+                'border-blue-500 bg-blue-500 text-white': index <= currentStep
+              }"
+              :data-test="`step-circle-${index}`"
+            >
+              <p class="px-3 py-[5px]">
+                {{ index + 1 }}
+              </p>
+            </div>
+          </slot>
+          <slot
+            name="content"
+            :index="index"
+            :currentStep="currentStep"
+            :title="step.title"
+            :subtitle="step.subtitle"
           >
-            <p class="px-3 py-[5px]">
-              {{ id + 1 }}
-            </p>
-          </div>
-          <div class="text-center">
-            <div
-              v-if="step.title"
-              class="whitespace-nowrap"
-              :class="{ 'font-bold': id === currentStep }"
-            >
-              {{ step.title }}
+            <div class="text-center" :data-test="`step-content-${index}`">
+              <div
+                v-if="step.title"
+                class="whitespace-nowrap"
+                :class="{ 'font-bold': index === currentStep }"
+              >
+                {{ step.title }}
+              </div>
+              <div
+                v-if="step.subtitle"
+                class="whitespace-nowrap"
+                :class="{ 'font-bold': index === currentStep }"
+              >
+                {{ step.subtitle }}
+              </div>
             </div>
-            <div
-              v-if="step.subtitle"
-              class="whitespace-nowrap"
-              :class="{ 'font-bold': id === currentStep }"
-            >
-              {{ step.subtitle }}
-            </div>
-          </div>
+          </slot>
         </div>
         <div
-          v-if="id < steps.length - 1"
+          v-if="index < steps.length - 1"
           class="h-0.5 w-full bg-gray-500"
           :class="{
-            '!bg-blue-500': id + 1 === currentStep
+            '!bg-blue-500': index + 1 === currentStep
           }"
+          :data-test="`step-divider-${index}`"
         ></div>
       </template>
     </div>
-    <slot name="content" :currentStep="currentStep">
-      <div class="p-10" v-if="activeStepContent">
+    <slot :currentStep="currentStep">
+      <div class="p-10" v-if="activeStepContent" data-test="active-step-content">
         <p>{{ activeStepContent }}</p>
       </div>
     </slot>
     <div class="px-10">
-        <button class="ml-5 border px-1" @click="prevStep">Previous</button>
-      <button class="border px-1" @click="nextStep">Next</button>
+      <button class="ml-5 border px-1" @click="prevStep" data-test="prev-button">Previous</button>
+      <button class="border px-1" @click="nextStep" data-test="next-button">Next</button>
     </div>
   </div>
 </template>
+
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
@@ -64,21 +78,22 @@ import { ref, computed } from 'vue'
 const { steps } = defineProps<{
   steps: { title: string; subtitle?: string; content?: string }[]
 }>()
+const emit = defineEmits(['next', 'prev'])
 
-// Change the initial value to 1
 const currentStep = ref(0)
 
 const nextStep = () => {
   if (currentStep.value < steps.length - 1) {
     currentStep.value++
   }
+  emit('next')
 }
 
 const prevStep = () => {
-  // Allow going to step 0
   if (currentStep.value > 0) {
     currentStep.value--
   }
+  emit('prev')
 }
 const activeStepContent = computed(() => {
   return steps[currentStep.value]?.content || ''
